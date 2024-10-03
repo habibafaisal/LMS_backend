dbac3a7ae54b8c956d3d80d6b9f397187f3d3644
habibafaisal <habibafaisal8@gmail.com>
2024-10-04 00:21:08 +0500

    basic file structure + db setup

---
+++ middleware/errorHandler.js
@@ -0,0 +1,52 @@
+const { constants } = require("../constants");
+
+const errorHandler = (err, req, res, next) => {
+  const statusCode = res.statusCode ? res.statusCode : 500;
+  switch (statusCode) {
+    case constants.NOT_FOUND:
+      res.json({
+        title: "Not Found Contact",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.VALIDATION_ERROR:
+      res.json({
+        title: "Validation Error",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.UNAUTHORIZED:
+      res.json({
+        title: "UNAUTHORIZED",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.FORBIDDEN:
+      res.json({
+        title: "FORBIDDEN",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    case constants.SERVER_ERROR:
+      res.json({
+        title: "SERVER_ERROR",
+        message: err.message,
+        stackTrace: err.stack,
+      });
+      break;
+
+    default:
+      console.log("no error");
+      break;
+  }
+};
+
+module.exports = errorHandler;
