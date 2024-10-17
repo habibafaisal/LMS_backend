b5d39fae079a31f78d867ae9fe525a0eef5a34ba
habibafa-lokal <habiba.faisal@lokal.pk>
2024-10-18 00:33:56 +0500

    Update server.js

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ import express from "express";
 import userRoutes from "./src/routes/userRoutes.js";
 import teacherRoutes from "./src/routes/teacherRoutes.js";
 import studentRoutes from "./src/routes/studentRoutes.js";
-import authroutes from "./src/routes/authroutes.js";
+import authRoutes from "./src/routes/authRoutes.js";
 import errorHandler from "./src/middleware/errorHandler.js";
 
 const app = express();
@@ -11,12 +11,7 @@ app.use(express.json());
 
 const PORT = process.env.PORT || 3000;
 
-// app.use(process.env.BASE_URL, authroutes);
-// app.use(process.env.BASE_URL + "/users/admin/", userRoutes);
-// app.use(process.env.BASE_URL + "/users/teacher/", teacherRoutes);
-// app.use(process.env.BASE_URL + "/users/student/", studentRoutes);
-
-app.use("/api/", authroutes);
+app.use("/api/", authRoutes);
 app.use("/api/users/admin/", userRoutes);
 app.use("/api/users/teacher/", teacherRoutes);
 app.use("/api/users/student/", studentRoutes);
